1cc890a97b329ae3d0f8e00fed7e6870198e4680
PuelMr <201931448@collegealma.ca>
2022-05-19 09:49:13 -0400

    Mise a jour du projet

--- src/main/java/org/calma/ETC/Controller.java
+++ src/main/java/org/calma/ETC/Controller.java
@@ -1,5 +1,6 @@
 package org.calma.ETC;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
@@ -7,13 +8,26 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.util.StringConverter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Controller {
     @FXML private TextField identification;
+
     @FXML private TextField prenom;
+    private String prenomValue;
+
     @FXML private TextField nom;
+    private String nomValue;
+
     @FXML private DatePicker date;
+    private String naissance;
+
     @FXML private TextField courriel;
+    private String valueCourriel;
+
     @FXML private ImageView image;
     @FXML private TextArea inclus;
     @FXML private TextArea exclus;
@@ -24,12 +38,33 @@ public class Controller {
     public void initialize(){
         validation.setDisable(true);
     }
+
     @FXML
     public void fullName(KeyEvent e) {
-        if(e.getSource() == prenom && prenom.getText().length() >= 3){
-
-        } else if (e.getSource() == nom && ){
-
+        if (e.getSource() == prenom) {
+            if (prenom.getText().length() >= 3) {
+                if (prenomValue.isEmpty()) prenomValue = prenom.getText().substring(0, 3);
+                setIdentification();
+            } else prenomValue = "";
+        }
+        if (e.getSource() == nom) {
+            if (nom.getText().length() >= 3) {
+                if (nomValue.isEmpty()) {
+                    nomValue = nom.getText().substring(0, 3);
+                    setIdentification();
+                } else nomValue = "";
+            }
         }
     }
+    @FXML
+    public void date(ActionEvent e) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-yy");
+        naissance = date.getValue().format(formatter);
+    }
+
+    private void setIdentification(){
+        identification.setText(nomValue + "-" + prenomValue + "-" + naissance);
+    }
+
+
 }
